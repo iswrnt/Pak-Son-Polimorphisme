96f75a3e1c1f81c0b11f1f522695689f352ae8e2
iswrnt <156394528+iswrnt@users.noreply.github.com>
2024-02-22 09:47:26 +0700

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,11 @@
+package com.mycompany.contohinterface;
+
+public interface Phone {
+    int MAX_VOLUME = 100;
+    int MIN_VOLUME = 0;
+    
+    void powerOn();
+    void powerOff();
+    void volumeUp();
+    void volumeDown();
+}
